844e07c5f791bc5a86e9759ca3c00d9f2db8d19e
Róger Corrente <139076362+rogercorrente@users.noreply.github.com>
2024-03-22 23:39:50 +0000

    criado o formulario de cliente e criado servidor com rota padrao

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+/// criando o servidor
+const express = require('express');
+const app = express();
+const porta = 9443;
+// Definindo rotas
+app.get('/', (req, res) =>{
+ res.send('Bem vindo ao cadastro de clientes.');
+});
+app.listen(porta, () => { console.log('Servidor rodando') });
+
+
 (async()=>{
   const database = require('./db');
-  const Cliente = require('./cliente');
+  const Cliente = require('./models/cliente');
   try{
     const resultado = await database.sync();
     console.log(resultado);
