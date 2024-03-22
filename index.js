fba3907089f60d57c2c642207ee0872c82cca991
Róger Corrente <139076362+rogercorrente@users.noreply.github.com>
2024-03-22 23:50:58 +0000

    setado as views, criado json para codificacao geral html e definida rota de cadastro do formulario cliente

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
+//importando bibliotecas e arquivos
+const database = require('./db');
+const Cliente = require('./models/cliente');
+
 /// criando o servidor
 const express = require('express');
 const app = express();
 const porta = 9443;
+const bodyParser = require('body-parser');
+//Setar os valores da view e engine
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile);
+app.use(bodyParser.json());
+app.use(express.urlencoded({extended: true}));
+
+
 // Definindo rotas
 app.get('/', (req, res) =>{
  res.send('Bem vindo ao cadastro de clientes.');
 });
+
+app.get("/cadcliente", function(req, res){
+  res.render('formCliente');
+})
+
+
 app.listen(porta, () => { console.log('Servidor rodando') });
 
 
 (async()=>{
-  const database = require('./db');
-  const Cliente = require('./models/cliente');
   try{
     const resultado = await database.sync();
     console.log(resultado);
