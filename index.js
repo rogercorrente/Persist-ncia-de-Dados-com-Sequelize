a0dc18eef2ae27782e78e76a89528682e357bf5f
Róger Corrente <139076362+rogercorrente@users.noreply.github.com>
2024-03-24 22:17:10 +0000

    POST - junção do front com back-end (cadastrando cliente)

--- index.js
+++ index.js
@@ -1,38 +1,49 @@
 //importando bibliotecas e arquivos
-const database = require('./db');
-const Cliente = require('./models/cliente');
+const database = require("./db");
+const Cliente = require("./models/cliente");
 
 /// criando o servidor
-const express = require('express');
+const express = require("express");
 const app = express();
 const porta = 9443;
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 //Setar os valores da view e engine
-app.set('view engine', 'html');
-app.engine('html', require('ejs').renderFile);
+app.set("view engine", "html");
+app.engine("html", require("ejs").renderFile);
 app.use(bodyParser.json());
-app.use(express.urlencoded({extended: true}));
-
+app.use(express.urlencoded({ extended: true }));
 
 // Definindo rotas
-app.get('/', (req, res) =>{
- res.send('Bem vindo ao cadastro de clientes.');
+app.get("/", (req, res) => {
+  res.send("Bem vindo ao cadastro de clientes.");
 });
 
-app.get("/cadcliente", function(req, res){
-  res.render('formCliente');
-})
+app.get("/cadcliente", function (req, res) {
+  res.render("formCliente");
+});
 
+app.post("/addcliente", function (req, res) {
+  Cliente.create({
+    nome: req.body.nome,
+    nascimento: req.body.nascimento,
+    cidade: req.body.cidade,
+    telefone: req.body.telefone,
+  }).then(function () {
+    res.send("Cliente cadastrado com sucesso!");
+  });
+});
 
-app.listen(porta, () => { console.log('Servidor rodando') });
+app.listen(porta, () => {
+  console.log("Servidor rodando");
+});
 
-
-(async()=>{
-  try{
+(async () => {
+  try {
     const resultado = await database.sync();
     console.log(resultado);
-  } catch(error){
+    const clientes = await Cliente.findAll();
+    console.log("Lista de clientes \n", clientes);
+  } catch (error) {
     console.log(error);
   }
-})
-();
+})();
--- models/cliente.js
+++ models/cliente.js
@@ -29,4 +29,4 @@ const Cliente = database.define('cliente', {
     allowNull: false
   }
 })
-module_exports = Cliente
+module.exports = Cliente
